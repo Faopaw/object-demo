296e3142fe16e9fa3802b59b82b65b5246ba3aaa
Leo Ho <leo.ho@mindera.com>
2022-08-04 21:06:10 +0100

    Exercise created:  teacher.js

---
+++ teacher.js
@@ -0,0 +1,22 @@
+/*
+  1. Teacher -- extends Person
+      - name obj
+         + firstName
+         + lastName
+      - subjects array
+         e.g., ['Math', 'English', 'Geography']
+      - learnSubject(): to add a new subject to subjects
+      - getFirstSubject(): to get the first subject in subjects
+
+  2. Person --
+      - name obj
+        + firstName
+        + lastName
+ 
+  3. Student -- extends Person
+      - name obj
+         + firstName
+         + lastName
+      - teachers array of teachers who teach him/her.
+      - addTeacher(): to add a new Teacher to teachers array.
+*/
